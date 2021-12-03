af4013a65cd40bc3d16094c35b8b1b1a0c9d798e
Reda <redanabil2199@gmail.com>
2021-12-03 17:03:15 +0100

    UpdateColor & animation

--- main.js
+++ main.js
@@ -1,17 +1,29 @@
 import './main.scss'
 
-let outerOrbitCircle = document.querySelector('.outer-orbit-first-circle')
+let outerFirstOrbitCircle = document.querySelector('.outer-orbit-first-circle')
 let middleOrbitCircle = document.querySelector('.middle-orbit-circle')
-let color = ["#FF5740", "#ECABA7 "];
+let innerOrbitCircle = document.querySelector('.inner-orbit-circle')
+let SecondOuterOrbitCircle = document.querySelector('.outer-orbit-second-circle')
+let color = ["#FF5740", "#ECABA7", "#E5C8CA", "#F97362"];
 
-const changeOuterOrbitCircleColor = () => {
+const changeFisrtOuterOrbitCircleColor = () => {
   //outerOrbitCircle.style.transition = "all 2s"
-  outerOrbitCircle.style.backgroundColor = "#FF5740"
+  outerFirstOrbitCircle.style.backgroundColor = "#FF5740"
 }
-
+const changeSecondOuterOrbitCircleColor = () => {
+  //middleOrbitCircle.style.transition = "all 2.5s"
+  SecondOuterOrbitCircle.style.backgroundColor = "#E5C8CA"
+}
 const changeMiddleOrbitCircleColor = () => {
   //middleOrbitCircle.style.transition = "all 2.5s"
   middleOrbitCircle.style.backgroundColor = "#ECABA7"
 }
-setInterval(changeOuterOrbitCircleColor, 2000);
+const changeInnerOrbitCircleColor = () => {
+  //middleOrbitCircle.style.transition = "all 2.5s"
+  innerOrbitCircle.style.backgroundColor = "#F97362"
+}
+
+setInterval(changeFisrtOuterOrbitCircleColor, 1500);
+setInterval(changeSecondOuterOrbitCircleColor, 3000);
 setInterval(changeMiddleOrbitCircleColor, 2500);
+setInterval(changeInnerOrbitCircleColor, 2000);
