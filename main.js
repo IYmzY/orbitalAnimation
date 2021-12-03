76ff949d9a6ac76caca2b43e5786bcef6cde6e37
Reda <redanabil2199@gmail.com>
2021-12-03 16:00:26 +0100

    animationColor

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
 import './main.scss'
+
+let outerOrbitCircle = document.querySelector('.outer-orbit-first-circle')
+let middleOrbitCircle = document.querySelector('.middle-orbit-circle')
+let color = ["#FF5740", "#DFE4EC"];
+let i = 0
+
+
+const changeOrbitCircleColor = () => {
+  //for (let i = 0; i < 3; i++) {
+  i = i + 1
+  if (i === 2) {
+    i = 0
+  }
+  outerOrbitCircle.style.transition = "all 1.5s"
+  outerOrbitCircle.style.backgroundColor = color[i]
+  //}
+}
+setInterval(changeOrbitCircleColor, 3000);
